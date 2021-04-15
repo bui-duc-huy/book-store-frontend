a4eb19ce8a090263e322ef3d60c9410363dd50b0
m2mforever <nevermind271999@gmail.com>
2021-04-15 23:31:18 +0700

    clean lan 1

--- src/components/section-title/SectionTitleWithText.js
+++ src/components/section-title/SectionTitleWithText.js
@@ -11,13 +11,12 @@ const SectionTitleWithText = ({ spaceTopClass, spaceBottomClass }) => {
       <div className="container">
         <div className="welcome-content text-center">
           <h5>Who Are We</h5>
-          <h1>Welcome To Flone</h1>
+          <h1>Welcome To Bookier</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt labor et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commo consequat irure{" "}
+          Appeared with the need to buy books more and more of everyone, especially with students. Bookier strives to bring you the best shopping experience, as well as the most comfortable price.
+Free shipping, free bookcare.{" "}
           </p>
+          <p>Every Books - Every Bookiers</p>
         </div>
       </div>
     </div>
